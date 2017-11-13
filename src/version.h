57fd4f4869d37c529a27ca18655f59c7bde7aff4
Flapmin <Flapmin@Gmail.com>
2017-11-13 01:03:46 -0500

    Soft Fork

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ static const int DATABASE_VERSION = 70001;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 85005;
+static const int PROTOCOL_VERSION = 90000;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
