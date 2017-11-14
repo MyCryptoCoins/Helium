29000e6ec87e0d63651bde9c10acfe178029b392
Flapmin <Flapmin@Gmail.com>
2017-11-14 04:30:53 -0500

    upper fork

--- src/version.h
+++ src/version.h
@@ -30,13 +30,13 @@ static const int DATABASE_VERSION = 70001;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 90000;
+static const int PROTOCOL_VERSION = 250000;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 85003;
+static const int MIN_PEER_PROTO_VERSION = 89999;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -44,7 +44,7 @@ static const int CADDR_TIME_VERSION = 30000;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 0;
-static const int NOBLKS_VERSION_END = 85001;
+static const int NOBLKS_VERSION_END = 89999;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
